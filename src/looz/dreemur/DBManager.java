8da7e8c70bad3a4d7186d2c892c0e720b4d0ef3d
Tiffceet <zkang.loo@gmail.com>
2019-11-30 20:02:07 +0800

    First Commit

---
+++ src/looz/dreemur/DBManager.java
@@ -0,0 +1,214 @@
+package looz.dreemur;
+
+import java.sql.*;
+import java.io.*;
+import javax.swing.JOptionPane;
+
+public class DBManager {
+
+    File src_DB, db;
+    Connection conn;
+    MainFrame mf;
+    int status;
+    String DB_schema, src_DB_path, db_path;
+
+    public DBManager(MainFrame mf, File db) {
+
+        // Defaults
+        status = 0;
+        DB_schema = "CREATE TABLE android_metadata (locale TEXT)\n"
+                + "CREATE TABLE music_playlist (_id INTEGER PRIMARY KEY autoincrement,name TEXT NOT NULL,path TEXT,album_pic TEXT)\n"
+                + "CREATE TABLE sqlite_sequence(name,seq)\n";
+        conn = null;
+
+        // references
+        this.src_DB = db;
+        src_DB_path = src_DB.getAbsolutePath();
+        this.mf = mf;
+
+        // DB startup operations
+        connectDatabase(src_DB);
+        verifyDB();
+
+        // verify if db is OK before proceeding
+        if (status == 1) {
+            return;
+        }
+
+        // Close database connection to source DB
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // Create a new DB file to be edited in process
+        db = new File("ongoing.db");
+        db_path = db.getAbsolutePath();
+
+        // copy the source DB to ongoing.db
+        try {
+            this.copyFileUsingStream(src_DB, db);
+
+            // connect to ongoing.db
+            connectDatabase(db);
+            // verify DB once again to check if file is copied correctly
+            verifyDB();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void connectDatabase(File dbfileToBeConnected) {
+        try {
+            String url = "jdbc:sqlite:" + dbfileToBeConnected.getAbsolutePath().replace("\\", "/");
+            // System.out.println("URL: " + url);
+            conn = DriverManager.getConnection(url);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "Connection to .db failed", "Error", JOptionPane.ERROR_MESSAGE);
+            status = 1;
+        }
+
+    }
+
+    // use this only when user tries to save
+    public void overwriteSourceDatabase() throws IOException {
+        try {
+            conn.close();
+
+            // redefine both of them as stated in copyFileUsingStream()
+            db = new File(db_path);
+            src_DB = new File(src_DB_path);
+
+            copyFileUsingStream(db, src_DB);
+
+            connectDatabase(db);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "Something went really wrong, please check overwriteSourceDatabase()", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "File Not Found error, pls dont mess with the file when saving.", "Error", JOptionPane.ERROR_MESSAGE);
+
+        }
+
+    }
+    
+    // use this only when user tries to save
+    public void overwriteSourceDatabase(String src_db_path) throws IOException {
+        try {
+            conn.close();
+
+            // redefine both of them as stated in copyFileUsingStream()
+            db = new File(db_path);
+            src_DB = new File(src_db_path);
+
+            copyFileUsingStream(db, src_DB);
+
+            connectDatabase(db);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "Something went really wrong, please check overwriteSourceDatabase()", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "File Not Found error, pls dont mess with the file when saving.", "Error", JOptionPane.ERROR_MESSAGE);
+
+        }
+
+    }
+
+    /*
+    public void createNewDatabase(String fileName) {
+
+        String url = "jdbc:sqlite:C:/Users/Acer/Desktop/db/" + fileName;
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+     */
+    public void verifyDB() {
+        try {
+
+            // checks schema *(shit way)
+            String q = "SELECT name, sql FROM sqlite_master "
+                    + "WHERE type='table' "
+                    + "ORDER BY name;";
+            ResultSet rs = getData(q);
+
+            String schema = "";
+
+            while (rs.next()) {
+                schema = schema + rs.getString("sql") + "\n";
+            }
+
+            if (!schema.equals(DB_schema)) {
+                throw new Exception("Validation error: Database schema doesnt match");
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "An error occured during database verification", "Error", JOptionPane.ERROR_MESSAGE);
+            status = 1;
+        }
+    }
+
+    public ResultSet getData(String sql) throws SQLException {
+        return conn.createStatement().executeQuery(sql);
+    }
+
+    public void execQuery(String sql) throws SQLException {
+        Statement st = conn.createStatement();
+        st.executeUpdate(sql);
+        st.close();
+    }
+
+    // will be used in future as statement in getData() is not handled properly, duh
+    public void freeUpMemory() throws SQLException {
+        if (conn != null) {
+            conn.close();
+        }
+        db = new File(db_path);
+        src_DB = new File(src_DB_path);
+
+        connectDatabase(db);
+    }
+
+    // copy file
+    private void copyFileUsingStream(File source, File dest) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+        } finally {
+            // Assumption 1: file is closed when its stream is closed
+            // Assumption 2: file is closed when connection is closed
+            // NOTE: whenever "db" and "src_DB" needs to be reaccess,
+            // NOTE: OR 
+            // NOTE: whenever "conn" is closed using conn.close()
+            // NOTE: redefine both of them using "new File(db_path)" and "new File(src_DB_path)"
+            if (is != null) {
+                is.close();
+            }
+            if (os != null) {
+                os.close();
+            }
+        }
+    }
+}
---
+++ src/looz/dreemur/WPLReader.java
@@ -0,0 +1,98 @@
+package looz.dreemur;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+class WPLReader {
+
+    String playlist;
+    ArrayList<String> songs;
+    File FILE_input;
+    int status;
+
+    public WPLReader(File in) {
+        this.FILE_input = in;
+        songs = new ArrayList<String>();
+        status = 0;
+        read();
+    }
+
+    private void read() {
+        if (!FILE_input.exists() || !FILE_input.getAbsoluteFile().getName().toLowerCase().endsWith(".wpl")) {
+            status = 1;
+            return;
+        }
+        try {
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(FILE_input), "UTF8"));
+
+            String str;
+
+            while ((str = in.readLine()) != null) {
+                str = str.trim();
+                // if its title
+                if (str.startsWith("<title>")) {
+                    if (str.length() == 15) {
+                        playlist = "Empty Playlist";
+                    } else {
+                        playlist = find_TITLE(str);
+                    }
+                }
+                // if its a media
+                if (str.startsWith("<media")) {
+                    // System.out.println("strval: " + str);
+                    String src = find_SRC(str);
+                    songs.add(getSongName_fromSRC(src));
+                }
+
+            }
+            in.close();
+        } catch (UnsupportedEncodingException e) {
+            System.out.println(e.getMessage());
+            status = 1;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            status = 1;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            status = 1;
+        }
+    }
+    
+    // New bug: source dont always ends with mp3
+    // Suggested fix: let find_SRC() function add additional " symbol to the end
+    //                and use that as ending indicator in this function
+    private String getSongName_fromSRC(String src) {
+        String songName = "";
+        songName = src.substring(src.lastIndexOf("\\") + 1, src.indexOf("\""));
+        return songName;
+    }
+
+    // Known Bug: if tid doesn't exist, it cause wpl import fail (unreported exception)
+    // Bug status: fixed
+    // Solution: find next " symbol after the beginning of found src
+    private String find_SRC(String in) {
+        String src = "";
+        // System.out.println("idxofSRC: " + in.indexOf("src=\"") + "\nidxOftid: " + in.indexOf("tid"));
+        // src = in.substring(in.indexOf("src=\"") + 5, in.indexOf("tid") - 2);
+        src = in.substring(in.indexOf("src=\"") + 5, in.length());
+        src = src.substring(0, src.indexOf("\"") + 1); // + 1 because getSongName_fromSRC() needs indicator for the end of song name
+        System.out.println("srcval: " + src);
+        
+        return src;
+    }
+
+    private String find_TITLE(String in) {
+        String title = "";
+        title = in.substring(in.indexOf("<title>") + 7, in.indexOf("</title>"));
+        return title;
+    }
+}
